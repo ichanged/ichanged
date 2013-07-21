2cdf5f19e5fbbabea6d0e37e73633d6e7e436d6b
leeyiw <leeyiw@gmail.com>
2013-07-21 13:31:00 +0800

    fix ignore hidden file

--- monitor.cpp
+++ monitor.cpp
@@ -126,7 +126,7 @@ monitor::do_init_monitor(const char *fpath, const struct stat *sb,
 	int wd;
 
 	// 如果配置了不监控隐藏文件与目录，则直接返回
-	if(options::watch_hidden && monitor::is_path_hidden(fpath)) {
+	if(!options::watch_hidden && monitor::is_path_hidden(fpath)) {
 		return 0;
 	}
 
@@ -158,7 +158,7 @@ monitor::do_add_monitor(const char *fpath, const struct stat *sb,
 	int wd;
 
 	// 如果配置了不监控隐藏文件与目录，则直接返回
-	if(options::watch_hidden && monitor::is_path_hidden(fpath)) {
+	if(!options::watch_hidden && monitor::is_path_hidden(fpath)) {
 		return 0;
 	}
 
@@ -193,7 +193,7 @@ monitor::is_path_hidden(const char *fpath)
 	while(slash != NULL) {
 		slash = strchr(slash, '/');
 		if(slash != NULL) {
-			if(slash[1] == '.' && options::watch_hidden) {
+			if(slash[1] == '.') {
 				return true;
 			} else {
 				slash++;
