ce5c2e0648b915f5614ec0d5bd51f22c498899c6
fayewu <fayewu37@gmail.com>
2013-08-29 23:39:02 +0800

    xx

--- test/test.cpp
+++ test/test.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+#include <cstdio>
 #include <errno.h>
 #include <cstdlib>
 #include <sys/inotify.h>
+#include <ftw.h>
 
-const string directory = "/home/wufei/ichanged"; 
+const EVENT_MAX = sizeof(struct inotify_event) + MAX + 1;
+const std::string directory = "/home/faye/ichanged"; 
+
+
+int inotify_fd;
+int mask;
+std::vector<int> wd_array; 
 
 typedef int (*ftw_func) (const char *fpath, const struct stat *sb, 
 		int typeflag);
@@ -11,22 +19,31 @@ typedef int (*ftw_func) (const char *fpath, const struct stat *sb,
 int
 add_monitor(const char *fpath, const struct stat *sb, int typeflag)
 {
-	
+	int wd;
+
+	if (typeflag == FTW_D) {
+		wd = inotify_add_watch(inotify_fd, fpath, mask);
+	}
+
+	return 0;
 }
 
 int
 main(int argc, char *argv[])
 {
-	int inotify_fd;
 	int retval;
-	int mask = IN_CREATE | IN_ATTRIB | IN_MODIFT | IN_DELETE;
+	mask = IN_CREATE | IN_ATTRIB | IN_MODIFY | IN_DELETE;
 
 	inotify_fd = inotify_init();
 
-	retval = ftw(directory.c_str(), (ftw_func)add_monitor);
+	retval = ftw(directory.c_str(), (ftw_func)add_monitor, 500);
 	if (retval == -1) {
 		perror("ftw error");
 		exit(EXIT_FAILURE);
+	}
+
+	while (true) {
+			
 	}
 
 	return 0;	
