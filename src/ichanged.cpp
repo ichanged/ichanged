b5ec7e69777581aeb045ebac7f9beae23284cdd6
fayewu <fayewu37@gmail.com>
2013-09-02 17:51:13 +0800

    add ichanged.cpp

---
+++ src/ichanged.cpp
@@ -0,0 +1,22 @@
+#include "monitor.h"
+#include "watcher.h"
+#include "options.h"
+#include "window.h"
+
+int
+main(int argc, char *argv[])
+{
+	/* 解析命令行参数 */
+	options::parse_args(argc, argv);
+
+	monitor::init();
+//	window::init();
+
+	monitor::wait();
+//	window::wait();
+
+	monitor::destory();
+//	window::destory();
+
+	return 0;
+}
