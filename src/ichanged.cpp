74f3a7b8401f99339d0eea631a8e60edde6fb810
fayewu <fayewu37@gmail.com>
2013-09-04 23:13:46 +0800

    add signal handler

--- src/ichanged.cpp
+++ src/ichanged.cpp
@@ -3,9 +3,33 @@
 #include "options.h"
 #include "window.h"
 
+static void destroy();
+
+void
+destroy()
+{
+	printf("hello world\n");
+	monitor::destroy();	
+	//window::destroy();
+}
+
 int
 main(int argc, char *argv[])
 {
+	int status;
+	struct sigaction sa;
+
+	memset(sa, 0, sizeof(sa));
+	sa.sa_handler = destroy;
+	status = sigaction(SIGINT, &sa, NULL);
+	if (status == -1 ) {
+		logger::warn("signal handler register error");
+	}
+
+	status = atexit(destory);
+	if (status != 0) {
+		logger::warn("exit handler register");
+	}
 	/* 解析命令行参数 */
 	options::parse_args(argc, argv);
 
