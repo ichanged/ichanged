aa119f0dc557cdcfff6d4956f105a510c762a389
fayewu <fayewu37@gmail.com>
2013-10-06 19:48:52 +0800

    fix bugs with dir delete

---
+++ src/record.cpp
@@ -0,0 +1,41 @@
+#include "options.h"
+#include "watcher.h"
+#include "event.h"
+#include "record.h"
+
+std::ofstream record::file; 
+
+void
+record::init()
+{
+	record::file.open(options::filename.c_str(), std::ios::app);
+}
+	
+void
+record::get_time()
+{
+	char tmp[1000] = {0};
+	time_t now;		
+	struct tm date;
+
+	now = time(NULL);
+	localtime_r(&now, &date);
+
+	record::file << "\n=========================================================\n";
+	sprintf(tmp, "%d-%02d-%02d %02d:%02d:%02d\n", date.tm_year + 1900,
+			date.tm_mon + 1, date.tm_mday, date.tm_hour, 
+			date.tm_min, date.tm_sec);	
+	record::file << tmp;
+}
+
+void
+record::event_to_file(char *output)
+{
+	record::file << output;
+}
+
+void
+record::close()
+{
+	record::file.close();	
+}
---
+++ src/record.h
@@ -0,0 +1,19 @@
+#ifndef _RECORD_H_
+#define _RECORD_H_
+
+#include <fstream>
+#include <string>
+#include <time.h>
+#include <stdio.h>
+
+class record {
+public:
+	static void init();
+	static void get_time(); 
+	static void event_to_file(char *output);	
+	static void close();
+
+	static std::ofstream file;
+};
+
+#endif
