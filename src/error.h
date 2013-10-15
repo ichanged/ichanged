94a5e39a5049dc684b156b5bce705b701abe5c2b
fayewu <fayewu37@gmail.com>
2013-10-15 22:55:39 +0800

    add error

--- src/error.h
+++ src/error.h
@@ -1,15 +1,20 @@
 #ifndef _ERROR_H_
 #define _ERROR_H_
 
+#include <string>
 #include <exception>
 
-class ichanged_error {
-	
+class Error {
 public:
-	ichanged_error(const std::string& msg);
-	string &what();
+	Error(std::string file, int line, const char *format, ...) throw();
+	std::string what();
+	std::string get_file();
+	int get_line();
 
 	std::string error_msg;
+private:
+	std::string file;
+	int line;
 };
 
 #endif
