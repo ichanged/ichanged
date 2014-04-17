0e5f055c89aff991e938a0e57beac470440be13c
fayewu <fayewu37@gmail.com>
2014-04-17 23:27:34 +0800

    hash

--- src/watcher.h
+++ src/watcher.h
@@ -13,46 +13,48 @@
 #include "watch.h"
 #include "record.h"
 
-class watcher {
-public:
-	static bool is_watch_exist(std::string path);
+void int init_watch(const struct stat *sb, char *path, int linked);
 
-	static watch *get_watch(int wd);
-	static int init_watch(const struct stat *sb, std::string path,
-			bool linked = false);
-	static int add_watch(const struct stat *sb, std::string path,
-			bool link = false, bool _linked = false);
-	static void remove_watch(int wd);
-
-	static void dir_attrib(int wd, char *path);
-	static void dir_delete(int wd, char *path);
-	static void dir_delete_datum(int wd);
-	static void dir_modify(int wd, char *path);
-
-	static void init_file(const struct stat *sb, std::string path); 
-	static void add_file(const struct stat *sb, std::string path);
-	static void file_create(int wd, std::string name);
-	static void file_attrib(int wd, std::string name);
-	static void file_modify(int wd, std::string name);
-	static void file_delete(int wd, std::string name);
-	static void file_write(int wd, std::string name);
-
-	static void export_file();
-	static std::vector<event> *generate_snapshot();
-	static void check_delete();
-	static void print();
-
-	static void lock();
-	static void unlock();
-
-	static std::map<std::string, int> _wd_map;
-private:
-	static std::map<int, watch> _watch_map;
-	static std::set<int> _watch_set;
-
-	static std::vector<event> _event_vec;
-
-	static pthread_mutex_t mutex;
-};
+//class watcher {
+//public:
+//	static bool is_watch_exist(std::string path);
+//
+//	static watch *get_watch(int wd);
+//	static int init_watch(const struct stat *sb, std::string path,
+//			bool linked = false);
+//	static int add_watch(const struct stat *sb, std::string path,
+//			bool link = false, bool _linked = false);
+//	static void remove_watch(int wd);
+//
+//	static void dir_attrib(int wd, char *path);
+//	static void dir_delete(int wd, char *path);
+//	static void dir_delete_datum(int wd);
+//	static void dir_modify(int wd, char *path);
+//
+//	static void init_file(const struct stat *sb, std::string path); 
+//	static void add_file(const struct stat *sb, std::string path);
+//	static void file_create(int wd, std::string name);
+//	static void file_attrib(int wd, std::string name);
+//	static void file_modify(int wd, std::string name);
+//	static void file_delete(int wd, std::string name);
+//	static void file_write(int wd, std::string name);
+//
+//	static void export_file();
+//	static std::vector<event> *generate_snapshot();
+//	static void check_delete();
+//	static void print();
+//
+//	static void lock();
+//	static void unlock();
+//
+//	static std::map<std::string, int> _wd_map;
+//private:
+//	static std::map<int, watch> _watch_map;
+//	static std::set<int> _watch_set;
+//
+//	static std::vector<event> _event_vec;
+//
+//	static pthread_mutex_t mutex;
+//};
 
 #endif
